e6fff392f508867043bd64ec2f258ea937769aed
hyeonseok <startwo09@gmail.com>
2024-07-03 21:54:33 +0900

    Chore: 로그인 모듈 생성

---
+++ feature/login/src/main/java/com/alreadyoccupiedseat/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.alreadyoccupiedseat.login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            com.alreadyoccupiedseat.designsystem.ShowPotTheme {
+
+            }
+        }
+    }
+}
