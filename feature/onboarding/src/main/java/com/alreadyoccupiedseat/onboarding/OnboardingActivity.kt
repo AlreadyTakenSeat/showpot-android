35c787bdedd6f4fb468a2a9ab38b147cf07555f4
soopeach <hsjeon584@gmail.com>
2024-07-03 14:04:56 +0900

    Chore: onboarding 모듈 추가

---
+++ feature/onboarding/src/main/java/com/alreadyoccupiedseat/onboarding/OnboardingActivity.kt
@@ -0,0 +1,26 @@
+package com.alreadyoccupiedseat.onboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.alreadyoccupiedseat.designsystem.ShowPotTheme
+
+class OnboardingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShowPotTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
