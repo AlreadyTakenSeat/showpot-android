cd1c9d41fc7feabd8a4d30191f8946a01296db49
soopeach <hsjeon584@gmail.com>
2024-07-04 15:11:42 +0900

    Feat: 아이콘을 가지는 쇼팟 버튼 스팩 수정

--- core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/component/ShowPotMainButtonWithIcon.kt
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/component/ShowPotMainButtonWithIcon.kt
@@ -1,12 +1,13 @@
 package com.alreadyoccupiedseat.designsystem.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,14 +40,16 @@ fun ShowPotMainButtonWithIcon(
         shape = RectangleShape,
         onClick = onClicked,
     ) {
-        Row {
-            Icon(
+        Row(
+            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+        ) {
+            Image(
                 painter = icon,
                 contentDescription = null,
-                tint = Color.Unspecified,
-                modifier = Modifier
-                    .padding(end = 8.dp)
             )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
             ShowPotKoreanText_H2(
                 text = text,
                 color = if (enabled) Color.Black else ShowpotColor.Gray400
