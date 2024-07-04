ad33d1d5eaa2a280a7cec9cbd791b915bd2853f6
soopeach <hsjeon584@gmail.com>
2024-07-04 14:34:01 +0900

    Feat: 온보딩 여부 확인용 DataStore 생성

--- core/datastore/src/main/java/com/alreadyoccupiedseat/datastore/OnboardingDataStore.kt
+++ core/datastore/src/main/java/com/alreadyoccupiedseat/datastore/OnboardingDataStore.kt
@@ -3,27 +3,28 @@ package com.alreadyoccupiedseat.datastore
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 val Context.onboardingDataStore: DataStore<Preferences> by preferencesDataStore(name = "onboarding")
 
-class OnboardingDataStore(private val context: Context) {
-    private val whetherIsFinishedKey = stringPreferencesKey("whetherIsFinishedKey")
+class OnboardingDataStore @Inject constructor(@ApplicationContext private val context: Context) {
+    private val whetherIsFinishedKey = booleanPreferencesKey("whetherIsFinishedKey")
 
-    private suspend fun updateWhetherOnboardingIsFinished(accessToken: String) {
+    suspend fun updateWhetherOnboardingIsFinished(isFinished: Boolean) {
         context.onboardingDataStore.edit { preferences ->
-            preferences[whetherIsFinishedKey] = accessToken
+            preferences[whetherIsFinishedKey] = isFinished
         }
     }
 
     suspend fun getWhetherIsFinished() = context.onboardingDataStore.data
         .map { preferences ->
             preferences[whetherIsFinishedKey]
-        }.first()
+        }.first() ?: false
 
 }
