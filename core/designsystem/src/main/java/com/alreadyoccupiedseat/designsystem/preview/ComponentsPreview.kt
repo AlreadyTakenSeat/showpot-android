320f2c1a2c2dc578767de4e438b9f71493437c9a
hyeonseok <startwo09@gmail.com>
2024-07-04 23:04:17 +0900

    Feat: ComponentsPreview 카카오/구글 버튼 Preview 구현 추가

--- core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/preview/ComponentsPreview.kt
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/preview/ComponentsPreview.kt
@@ -2,12 +2,20 @@ package com.alreadyoccupiedseat.designsystem.preview
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ButtonColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.alreadyoccupiedseat.designsystem.R
+import com.alreadyoccupiedseat.designsystem.ShowpotColor
+import com.alreadyoccupiedseat.designsystem.component.ShowPotButtonWithIcon
 import com.alreadyoccupiedseat.designsystem.component.ShowPotMainButton
 import com.alreadyoccupiedseat.designsystem.typo.korean.ShowPotKoreanText_H0
 import com.alreadyoccupiedseat.designsystem.typo.korean.ShowPotKoreanText_H1
@@ -66,6 +74,51 @@ fun ComponentsPreview() {
                 text = "disabled Main Button With Padding",
                 enabled = false,
                 onClicked = {})
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        item {
+            ShowPotButtonWithIcon(
+                modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .height(55.dp),
+                text = stringResource(R.string.button_login_with_kakao),
+                icon = painterResource(R.drawable.ic_kakao),
+                colors = ButtonColors(
+                    containerColor = ShowpotColor.Kakao,
+                    contentColor = Color.Black,
+                    disabledContainerColor = ShowpotColor.Gray600,
+                    disabledContentColor = ShowpotColor.Gray400,
+                ),
+                onClick = {
+                    /* 카카오 로그인 */
+                }
+            )
+        }
+        item {
+            ShowPotButtonWithIcon(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        vertical = 8.dp,
+                        horizontal = 20.dp,
+                    )
+                    .height(55.dp),
+                text = stringResource(R.string.button_login_with_google),
+                icon = painterResource(R.drawable.ic_google),
+                colors = ButtonColors(
+                    containerColor = ShowpotColor.White,
+                    contentColor = Color.Black,
+                    disabledContainerColor = ShowpotColor.Gray600,
+                    disabledContentColor = ShowpotColor.Gray400,
+                ),
+                onClick = {
+                    /* 구글 로그인 */
+                }
+            )
         }
     }
 }
