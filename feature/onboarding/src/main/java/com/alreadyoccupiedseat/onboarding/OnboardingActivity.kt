37d760533ace27b202d65c68c0ca1bc3c9eda419
soopeach <hsjeon584@gmail.com>
2024-07-04 14:34:26 +0900

    Feat: 온보딩 페이지 구현

--- feature/onboarding/src/main/java/com/alreadyoccupiedseat/onboarding/OnboardingActivity.kt
+++ feature/onboarding/src/main/java/com/alreadyoccupiedseat/onboarding/OnboardingActivity.kt
@@ -1,14 +1,19 @@
 package com.alreadyoccupiedseat.onboarding
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.alreadyoccupiedseat.designsystem.ShowPotTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OnboardingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,11 @@ class OnboardingActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val context = this
+                    OnboardingScreen {
+                        // Todo: Navigate to the next screen
+                        Toast.makeText(context, "Finished", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
