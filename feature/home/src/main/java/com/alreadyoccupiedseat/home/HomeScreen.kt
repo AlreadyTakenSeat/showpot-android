e5bc5a8c1207221998651dd42c9284de1259ee79
hyeonseok <startwo09@gmail.com>
2024-07-19 00:54:50 +0900

    Chore: 장르/아티스트 구독하기 컴포넌트들 홈 스크린에 추가

--- feature/home/src/main/java/com/alreadyoccupiedseat/home/HomeScreen.kt
+++ feature/home/src/main/java/com/alreadyoccupiedseat/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,10 +31,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.alreadyoccupiedseat.core.extension.isScrollingUp
 import com.alreadyoccupiedseat.designsystem.R
 import com.alreadyoccupiedseat.designsystem.ShowpotColor
+import com.alreadyoccupiedseat.designsystem.component.ShowPotArtist
+import com.alreadyoccupiedseat.designsystem.component.ShowPotGenre
+import com.alreadyoccupiedseat.designsystem.component.ShowPotMenu
 import com.alreadyoccupiedseat.designsystem.component.ShowPotSearchBar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -52,6 +59,8 @@ fun HomeScreenContent() {
         remember { derivedStateOf { scrollState.firstVisibleItemIndex } }.value
 
     val context = LocalContext.current
+
+    val viewModel = viewModel<HomeViewModel>()
 
     // 첫 번째 아이템이 보이면 무조건 상단바 노출
     if (firstVisibleItemIndex == 0) {
@@ -96,7 +105,51 @@ fun HomeScreenContent() {
             }
 
             // item section
-
+            item {
+                ShowPotMenu(
+                    text = stringResource(id = R.string.subscribe_artist),
+                    endIcon = painterResource(id = R.drawable.ic_arrow_36_right),
+                )
+            }
+            item {
+                LazyRow(
+                    modifier = Modifier.padding(start = 16.dp, top = 8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    items(viewModel.genreList) { (resId, _) ->
+                        ShowPotGenre(icon = painterResource(id = resId))
+                    }
+                }
+            }
+            item {
+                ShowPotMenu(
+                    modifier = Modifier.padding(top = 36.dp),
+                    text = stringResource(id = R.string.subscribe_artist),
+                    endIcon = painterResource(id = R.drawable.ic_arrow_36_right),
+                )
+            }
+            item {
+                LazyRow(
+                    modifier = Modifier.padding(start = 16.dp, top = 6.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    items(count = 10) {
+                        ShowPotArtist(
+                            text = "High Flying Birds",
+                            icon = painterResource(id = R.drawable.img_artist_default),
+                        )
+                    }
+                }
+            }
+            items(count = 10) { index ->
+                Spacer(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp, vertical = 10.dp)
+                        .background(if (index % 2 == 0) ShowpotColor.MainRed else ShowpotColor.MainGreen)
+                        .fillMaxWidth()
+                        .height(133.dp)
+                )
+            }
         }
 
         AnimatedVisibility(
