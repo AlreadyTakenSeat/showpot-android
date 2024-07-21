1189c0f91338f539bc83281fbf3f3694b41e0c8e
soopeach <hsjeon584@gmail.com>
2024-07-21 14:29:05 +0900

    Feat: 검색 화면 탑바 영역 구현

--- feature/search/src/main/java/com/alreadyoccupiedseat/search/SearchScreen.kt
+++ feature/search/src/main/java/com/alreadyoccupiedseat/search/SearchScreen.kt
@@ -1,11 +1,79 @@
 package com.alreadyoccupiedseat.search
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.alreadyoccupiedseat.designsystem.ShowpotColor
+import com.alreadyoccupiedseat.designsystem.component.ShowPotSearchBar
 
 @Composable
 fun SearchScreen(
     navController: NavController,
 ) {
+    SearchScreenContent() {
+        navController.popBackStack()
+    }
+}
 
+@Composable
+fun SearchScreenContent(
+    onBackClicked : () -> Unit = {}
+) {
+
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        containerColor = ShowpotColor.Gray700,
+        topBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 8.dp, end = 16.dp, top = 18.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.clickable {
+                        onBackClicked()
+                    },
+                    painter = painterResource(id = com.alreadyoccupiedseat.designsystem.R.drawable.ic_arrow_36_left),
+                    contentDescription = null,
+                    tint = ShowpotColor.Gray300,
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                ShowPotSearchBar(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    hint = stringResource(R.string.search_shows_and_artists_hint),
+                    inputText = text,
+                    onTextChanged = {
+                        text = it
+                    }
+                )
+            }
+        }
+    ) { paddingValues ->  
+
+    }
 }
