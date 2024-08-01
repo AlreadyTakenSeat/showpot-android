db31a6421329c38f1a057d10bd97f3c897ee8f57
soopeach <hsjeon584@gmail.com>
2024-08-01 13:24:43 +0900

    Feat: 로직을 제외한 아티스트 구독 화면 구현

--- feature/subscription-artist/src/main/java/com/alreadyoccupiedseat/subscription_artist/SubscriptionArtistScreen.kt
+++ feature/subscription-artist/src/main/java/com/alreadyoccupiedseat/subscription_artist/SubscriptionArtistScreen.kt
@@ -1,22 +1,32 @@
 package com.alreadyoccupiedseat.subscription_artist
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.alreadyoccupiedseat.designsystem.ShowpotColor
+import com.alreadyoccupiedseat.designsystem.component.ShowPotArtistSubscription
+import com.alreadyoccupiedseat.designsystem.component.ShowPotMainButton
 import com.alreadyoccupiedseat.designsystem.typo.korean.ShowPotKoreanText_H1
+import com.alreadyoccupiedseat.designsystem.typo.korean.ShowPotKoreanText_H2
 
 @Composable
 fun SubscriptionArtistScreen(
@@ -31,7 +41,8 @@ fun SubscriptionArtistScreen(
 
 @Composable
 fun SubscriptionArtistScreenContent(
-    onBackClicked: () -> Unit
+    onBackClicked: () -> Unit,
+    onSubscribeButtonClicked: () -> Unit = {},
 ) {
     Scaffold(
         modifier = Modifier
@@ -57,13 +68,64 @@ fun SubscriptionArtistScreenContent(
                 )
 
                 ShowPotKoreanText_H1(
-                    text = "아티스트 구독하기",
+                    text = stringResource(R.string.subscribe_artist),
                     color = ShowpotColor.Gray100,
                 )
             }
         }
 
-    ) {
+    ) { innerPadding ->
 
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+
+            ShowPotKoreanText_H2(
+                Modifier.padding(top = 5.dp, start = 16.dp),
+                text = stringResource(R.string.select_interesting_artist),
+                color = ShowpotColor.Gray300
+            )
+
+            Box(
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                LazyVerticalGrid(
+                    modifier = Modifier
+                        .padding(horizontal = 27.dp)
+                        .fillMaxSize()
+                        .padding(top = 20.dp),
+                    columns = GridCells.Fixed(3),
+                    horizontalArrangement = Arrangement.spacedBy(18.dp),
+                    verticalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    // TODO: Real Data
+                    items(count = 21) {
+                        ShowPotArtistSubscription(
+                            text = "High Flying Birds",
+                            icon = painterResource(id = com.alreadyoccupiedseat.designsystem.R.drawable.img_artist_default),)
+                    }
+
+                }
+
+                // Todo: Visibility depends on whether artists are selected
+                Box(
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .fillMaxWidth()
+                        .padding(bottom = 54.dp),
+                ) {
+                    ShowPotMainButton(
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp)
+                            .fillMaxWidth(),
+                        text = stringResource(R.string.subscribe)
+                    ) {
+                        onSubscribeButtonClicked()
+                    }
+                }
+            }
+        }
     }
 }
