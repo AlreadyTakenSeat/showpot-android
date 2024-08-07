4df8b53bd0a969a9ad63b25f037647f78fdb9e04
hyeonseok <startwo09@gmail.com>
2024-08-07 21:49:56 +0900

    Feat: MyAlarmSetting 모듈 init

---
+++ feature/myalarm-setting/src/main/java/com/alreadyoccupiedseat/myalarm_setting/MyAlarmSettingScreen.kt
@@ -0,0 +1,14 @@
+package com.alreadyoccupiedseat.myalarm_setting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyAlarmSettingScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun MyAlarmSettingScreenContent(modifier: Modifier = Modifier) {
+
+}
