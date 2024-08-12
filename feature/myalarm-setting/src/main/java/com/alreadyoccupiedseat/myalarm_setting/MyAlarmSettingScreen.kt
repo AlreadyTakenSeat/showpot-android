c7f67a88ad54acb6e0f51286450f03ee9669a3d9
hyeonseok <startwo09@gmail.com>
2024-08-12 23:11:00 +0900

    Chore: 내 알림 화면 분리한 TopBar, BottomSheet 컴포저블 사용으로 수정

--- feature/myalarm-setting/src/main/java/com/alreadyoccupiedseat/myalarm_setting/MyAlarmSettingScreen.kt
+++ feature/myalarm-setting/src/main/java/com/alreadyoccupiedseat/myalarm_setting/MyAlarmSettingScreen.kt
@@ -7,14 +7,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,13 +28,9 @@ import androidx.navigation.NavController
 import com.alreadyoccupiedseat.designsystem.R
 import com.alreadyoccupiedseat.designsystem.ShowpotColor
 import com.alreadyoccupiedseat.designsystem.component.ShowInfo
-import com.alreadyoccupiedseat.designsystem.component.ShowPotEmpty
-import com.alreadyoccupiedseat.designsystem.component.ShowPotTopBar
-import com.alreadyoccupiedseat.designsystem.component.bottomSheet.SheetHandler
-import com.alreadyoccupiedseat.designsystem.component.bottomSheet.ShowPotBottomSheet
+import com.alreadyoccupiedseat.designsystem.component.DefaultScreenWhenEmpty
 import com.alreadyoccupiedseat.designsystem.component.bottomSheet.TicketingNotificationBottomSheet
 import com.alreadyoccupiedseat.designsystem.component.button.ShowPotSubButton
-import com.alreadyoccupiedseat.designsystem.typo.korean.ShowPotKoreanText_H1
 
 @Composable
 fun MyAlarmSettingScreen(
@@ -172,86 +165,6 @@ fun MyAlarmSettingScreenContent(
 }
 
 @Composable
-fun MyAlarmSettingTopBar(
-    modifier: Modifier = Modifier,
-    onBackClicked: () -> Unit,
-) {
-    ShowPotTopBar(
-        navigationIcon = {
-            IconButton(onClick = { onBackClicked() }) {
-                Icon(
-                    tint = ShowpotColor.White,
-                    modifier = Modifier.padding(1.dp),
-                    painter = painterResource(id = R.drawable.ic_arrow_36_left),
-                    contentDescription = "Back"
-                )
-            }
-        },
-        title = {
-            ShowPotKoreanText_H1(
-                text = stringResource(id = R.string.alarm_settings_show),
-                color = ShowpotColor.Gray300,
-                modifier = Modifier
-                    .padding(start = 4.dp)
-                    .padding(vertical = 7.dp)
-            )
-        },
-        contentColor = ShowpotColor.Gray700,
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AlarmOptionsBottomSheet(
-    onTicketSheetVisible: () -> Unit,
-    onDismissRequest: () -> Unit,
-    onRemoveClicked: () -> Unit,
-) {
-    ShowPotBottomSheet(
-        onDismissRequest = onDismissRequest
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-            SheetHandler()
-
-            ShowPotKoreanText_H1(
-                text = stringResource(id = R.string.alarm),
-                color = ShowpotColor.White,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp)
-            )
-
-            ShowPotSubButton(
-                modifier = Modifier
-                    .padding(top = 16.dp),
-                text = stringResource(id = R.string.action_change),
-                onClicked = {
-                    onTicketSheetVisible()
-                    onDismissRequest()
-                }
-            )
-
-            ShowPotSubButton(
-                modifier = Modifier
-                    .padding(top = 12.dp),
-                text = stringResource(id = R.string.action_turn_off),
-                onClicked = {
-                    onRemoveClicked()
-                }
-            )
-
-            Spacer(modifier = Modifier.height(54.dp))
-        }
-    }
-}
-
-@Composable
 fun MyAlarmEmpty(modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
@@ -261,7 +174,7 @@ fun MyAlarmEmpty(modifier: Modifier = Modifier) {
             .fillMaxSize()
     ) {
 
-        ShowPotEmpty(
+        DefaultScreenWhenEmpty(
             imageResId = R.drawable.img_empty_alarm,
             text = stringResource(id = R.string.no_alarm_show)
         )
