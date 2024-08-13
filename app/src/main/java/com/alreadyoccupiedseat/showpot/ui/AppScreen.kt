d9e9e6d0e173fdcb1a6afe1a3b8d547519f37bf8
soopeach <hsjeon584@gmail.com>
2024-08-13 15:09:23 +0900

    Feat: 설정 화면 Screen의 data object로 분리

--- app/src/main/java/com/alreadyoccupiedseat/showpot/ui/AppScreen.kt
+++ app/src/main/java/com/alreadyoccupiedseat/showpot/ui/AppScreen.kt
@@ -127,7 +127,7 @@ fun AppScreenContent() {
                 MyAlarmSettingScreen(navController)
             }
 
-            composable("settings") {
+            composable(Screen.Settings.route) {
                 SettingsScreen(navController)
             }
         }
