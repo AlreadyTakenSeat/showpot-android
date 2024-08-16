74eac140eca864902649922c7146d03c216f48ed
soopeach <hsjeon584@gmail.com>
2024-08-16 18:07:44 +0900

    Feat: 알림 권한요청 적용

--- app/src/main/java/com/alreadyoccupiedseat/showpot/MainActivity.kt
+++ app/src/main/java/com/alreadyoccupiedseat/showpot/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.alreadyoccupiedseat.showpot
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.alreadyoccupiedseat.designsystem.ShowPotTheme
@@ -14,13 +16,19 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startService(Intent(this, ShowPotFcmService::class.java))
+
         setContent {
             // TODO: Improve the reactivity
             // ex) do not let them see the onboarding screen "at all" if they have already completed it
             ShowPotTheme {
+
+                EssentialPermissionDialog()
+
                 val viewModel = hiltViewModel<MainActivityViewModel>()
                 val state = viewModel.state.collectAsState()
 
