64172e5be8c574e2a7ea4078ae9e9b3c085fd401
hyeonseok <startwo09@gmail.com>
2024-08-27 01:04:37 +0900

    Chore: SubscriptionGenre Top/Bottom 가독성을 위해 파일 생성 컴포저블 함수 이동

---
+++ feature/subscription-genre/src/main/java/com/alreadyoccupiedseat/subscription_genre/SubscriptionGenreBottomSheet.kt
@@ -0,0 +1,53 @@
+package com.alreadyoccupiedseat.subscription_genre
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.alreadyoccupiedseat.designsystem.R
+import com.alreadyoccupiedseat.designsystem.component.ShowPotMainButton
+import com.alreadyoccupiedseat.designsystem.component.bottomSheet.SheetHandler
+import com.alreadyoccupiedseat.designsystem.component.bottomSheet.ShowPotBottomSheet
+import com.alreadyoccupiedseat.designsystem.typo.korean.ShowPotKoreanText_H1
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SubscriptionGenreBottomSheet(onDismissRequest: () -> Unit) {
+    ShowPotBottomSheet(
+        onDismissRequest = onDismissRequest,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SheetHandler()
+
+            ShowPotKoreanText_H1(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.login_subscribe_genre),
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(19.dp))
+
+            ShowPotMainButton(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.login_in_3_seconds)
+            ) {
+                // TODO: goto Login
+            }
+
+            Spacer(modifier = Modifier.height(54.dp))
+        }
+    }
+}
