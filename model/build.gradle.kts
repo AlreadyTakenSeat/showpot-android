850383b09fca8a74243d2d0b11450c1f3d2dfbee
hyeonseok <startwo09@gmail.com>
2024-08-26 21:02:23 +0900

    Chore: model module build.gradle.kts 수정

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -1,9 +1,29 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.showpot.library)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+android {
+    namespace = "com.alreadyoccupiedseat.model"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    } }
+
+dependencies {
+    implementation(project(":core:designsystem"))
+    implementation(libs.androidx.annotation)
 }
