cd58eca7344f075ea3330efd1dec4b6e31d2163d
hyeonseok <startwo09@gmail.com>
2024-08-28 21:44:27 +0900

    Chore: 장르 구독/취소 Api 추가

--- core/network/src/main/java/com/alreadyoccupiedseat/network/GenreService.kt
+++ core/network/src/main/java/com/alreadyoccupiedseat/network/GenreService.kt
@@ -1,10 +1,13 @@
 package com.alreadyoccupiedseat.network
 
-import com.alreadyoccupiedseat.model.Artist
 import com.alreadyoccupiedseat.model.Genre
 import com.alreadyoccupiedseat.model.PagingData
+import com.alreadyoccupiedseat.model.genre.SubscribeGenreRequest
+import com.alreadyoccupiedseat.model.genre.SubscribeGenreResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface GenreService {
@@ -12,5 +15,15 @@ interface GenreService {
     suspend fun getGenres(
         @Query("size") size: Int = 20
     ): Response<PagingData<Genre>>
+
+    @POST("/api/v1/genres/unsubscribe")
+    suspend fun unsubscribeGenres(
+        @Body genreIds: SubscribeGenreRequest
+    ): Response<SubscribeGenreResponse>
+
+    @POST("/api/v1/genres/subscribe")
+    suspend fun subscribeGenres(
+        @Body genreIds: SubscribeGenreRequest
+    ): Response<SubscribeGenreResponse>
 
 }
