fae221029c56a63fd3ffa9cc432e44604c6c6a36
hyeonseok <startwo09@gmail.com>
2024-08-28 21:54:35 +0900

    Chore: 장르 구독 화면에 필요한 state 수정 및 업데이트 함수들 추가

--- feature/subscription-genre/src/main/java/com/alreadyoccupiedseat/subscription_genre/SubscriptionGenreViewModel.kt
+++ feature/subscription-genre/src/main/java/com/alreadyoccupiedseat/subscription_genre/SubscriptionGenreViewModel.kt
@@ -8,8 +8,8 @@ import com.alreadyoccupiedseat.datastore.AccountDataStore
 import com.alreadyoccupiedseat.model.Genre
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,8 +20,13 @@ sealed interface SubscriptionGenreScreenEvent {
 data class SubscriptionGenreScreenState(
     val genres: List<Genre> = emptyList(),
     val selectedGenre: List<Genre> = emptyList(),
+    val subscribedGenre: List<Genre> = emptyList(),
+    val unSelectedGenre: Genre? = null,
     val isLoggedIn: Boolean = false,
-    val tempGenreList: List<Pair<Int, Int>> = emptyList(),
+    // Visible
+    val isLoginRequestBottomSheetVisible: Boolean = false,
+    val isSubscriptionSheetVisible: Boolean = false,
+    val isUnSubscriptionSheetVisible: Boolean = false,
 )
 
 @HiltViewModel
@@ -33,47 +38,74 @@ class SubscriptionGenreViewModel @Inject constructor(
     private val _state = MutableStateFlow(SubscriptionGenreScreenState())
     val state = _state.asStateFlow()
 
-    private val _event = MutableStateFlow(SubscriptionGenreScreenEvent.Idle)
-    val event = _event.asSharedFlow()
-
     init {
         getAllGenres()
         viewModelScope.launch {
-            accountDataStore.getAccessToken()?.let {
-                _state.value = _state.value.copy(isLoggedIn = true)
+            accountDataStore.getAccessTokenFlow().collectLatest { token ->
+                _state.value = _state.value.copy(isLoggedIn = token != null)
             }
         }
     }
 
-    // 임시
-    val tempGenreList = listOf(
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_rock to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_rock,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_band to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_band,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_edm to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_edm,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_classic to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_classic,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_hiphop to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_hiphop,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_house to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_house,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_opera to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_opera,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_pop to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_pop,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_rnb to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_rnb,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_musical to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_musical,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_metal to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_metal,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_jpop to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_jpop,
-        com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_jazz to com.alreadyoccupiedseat.designsystem.R.drawable.img_genre_selected_jazz,
-    )
-
-    fun subscribeGenre() {
+    /** 모든 장르 **/
+    private fun getAllGenres() {
         viewModelScope.launch {
-//            val genreId = state.value.selectedGenre.map { it.id }
-//            val subscribedArtistsIds = genreRepository.subscribeArtists(artistIds)
-//            _state.value = _state.value.copy(
-//                selectedGenre = emptyList(),
-//                unsubscribedGenres = state.value.unsubscribedGenres.filter { it.id !in subscribeArtists() },
-//            )
+            val result = genreRepository.getGenres(
+                size = 20,
+            )
+            Log.w("getAllGenres", "getAllGenres: $result")
+            _state.value = _state.value.copy(
+                genres = result,
+                subscribedGenre = result.filter { it.isSubscribed }
+            )
         }
     }
 
+    /** 장르를 구독 **/
+    fun subscribeGenre() {
+        viewModelScope.launch {
+            val genreIds = state.value.selectedGenre.map { it.id }
+            Log.d("subscribeGenre", "subscribeGenre: $genreIds")
+            genreRepository.subscribeGenres(genreIds).let {
+                // Add new subscribed genres to the list
+                val newSubscribedGenres =  state.value.subscribedGenre + state.value.genres.filter { genre -> genreIds.contains(genre.id) }
+                _state.value = _state.value.copy(
+                    selectedGenre = emptyList(),
+                    subscribedGenre = newSubscribedGenres
+                )
+            }
+        }
+    }
+
+    /** 장르 구독을 취소  **/
+    fun unSubscribeGenre() {
+        viewModelScope.launch {
+            state.value.unSelectedGenre?.id?.let { unSelectedGenreId ->
+                Log.d("unSubscribeGenre", "unSubscribeGenre: $unSelectedGenreId")
+                genreRepository.unsubscribeGenres(listOf(unSelectedGenreId)).let {
+                    _state.value = _state.value.copy(
+                        subscribedGenre = state.value.subscribedGenre.filter { it.id != unSelectedGenreId },
+                        unSelectedGenre = null,
+                        isUnSubscriptionSheetVisible = false
+                    )
+                }
+            }
+        }
+    }
+
+    /** 장르 구독 해제를 위한 장르를 선택 **/
+    fun selectUnSubscribeGenre(genre: Genre) {
+        Log.d("selectUnSubscribeGenre", "selectUnSubscribeGenre: $genre")
+        viewModelScope.launch {
+            _state.value = _state.value.copy(
+                unSelectedGenre = genre
+            )
+        }
+    }
+
+    /** 장르를 선택, 해제 **/
     fun selectGenre(genre: Genre) {
+        Log.d("selectGenre", "selectGenre: $genre")
         if (state.value.selectedGenre.contains(genre)) {
             _state.value = _state.value.copy(
                 selectedGenre = _state.value.selectedGenre - genre,
@@ -83,23 +115,25 @@ class SubscriptionGenreViewModel @Inject constructor(
                 selectedGenre = _state.value.selectedGenre + genre,
             )
         }
-
     }
 
+    /** 장르가 선택된 상태 확인 **/
     fun isSelected(genre: Genre): Boolean {
-        return state.value.selectedGenre.contains(genre)
+        return _state.value.selectedGenre.contains(genre)
     }
 
-    private fun getAllGenres() {
-        viewModelScope.launch {
-            val result = genreRepository.getGenres(
-                size = 20,
-            )
-            Log.w("getAllGenres", "getAllGenres: $result")
-            _state.value = _state.value.copy(
-                genres = result,
-            )
-        }
+    /** 구독 해제 시트 Visible **/
+    fun setUnSubscriptionSheetVisible(isVisible: Boolean) {
+        _state.value = _state.value.copy(
+            isUnSubscriptionSheetVisible = isVisible,
+        )
+    }
+
+    /** 로그인 요청 시트 Visible **/
+    fun setLoginRequestSheetVisible(isVisible: Boolean) {
+        _state.value = _state.value.copy(
+            isLoginRequestBottomSheetVisible = isVisible,
+        )
     }
 
 }
