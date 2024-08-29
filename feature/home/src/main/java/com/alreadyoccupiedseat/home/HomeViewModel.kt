e39a77a6756583b68905f00c280882f6d8c77546
hyeonseok <startwo09@gmail.com>
2024-08-29 23:31:19 +0900

    Chore: 홈 화면 티켓팅이 얼마 남지 않은 공연 api 연결

--- feature/home/src/main/java/com/alreadyoccupiedseat/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/alreadyoccupiedseat/home/HomeViewModel.kt
@@ -1,7 +1,13 @@
 package com.alreadyoccupiedseat.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alreadyoccupiedseat.data.show.ShowRepository
 import com.alreadyoccupiedseat.designsystem.R
+import com.alreadyoccupiedseat.model.show.Shows
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 // TODO 삭제
@@ -36,10 +42,20 @@ val performances = listOf(
     )
 )
 
-class HomeViewModel@Inject constructor() : ViewModel(){
+data class HomeScreenState(
+    val genreList: List<Pair<Int, Int>> = emptyList(),
+    val entireShowList: Shows = Shows(data = emptyList(), hasNext = false, size = 0),
+)
 
-    // 임시
-    val genreList = listOf(
+@HiltViewModel
+class HomeViewModel@Inject constructor(
+    private val showRepository: ShowRepository
+) : ViewModel(){
+
+    private val _state = MutableStateFlow(HomeScreenState())
+    val state = _state
+
+    private val genreList = listOf(
         R.drawable.img_genre_rock to R.drawable.img_genre_selected_rock,
         R.drawable.img_genre_band to R.drawable.img_genre_selected_band,
         R.drawable.img_genre_edm to R.drawable.img_genre_selected_edm,
@@ -53,5 +69,32 @@ class HomeViewModel@Inject constructor() : ViewModel(){
         R.drawable.img_genre_metal to R.drawable.img_genre_selected_metal,
         R.drawable.img_genre_jpop to R.drawable.img_genre_selected_jpop,
     )
+
+    init {
+        getEntireShow()
+        _state.value = _state.value.copy(
+            genreList = genreList
+        )
+    }
+
+    /** 전체 공연 목록 가져오기 ***/
+    private fun getEntireShow() {
+        viewModelScope.launch {
+            val tempRequestSize = 2
+            showRepository.getEntireShow(
+                sort = Shows.RECENT,
+                onlyOpenSchedule = false,
+                size = tempRequestSize,
+            ).let { result ->
+                _state.value = _state.value.copy(
+                    entireShowList = Shows(
+                        data = result,
+                        hasNext = true,
+                        size = result.size
+                    )
+                )
+            }
+        }
+    }
 
 }
