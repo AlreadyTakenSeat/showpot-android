9bee11985a1184ba39f71146f21255815b19d6e7
soopeach <hsjeon584@gmail.com>
2024-08-31 03:29:11 +0900

    Fix: 서치바에 곡률 적용

--- core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/component/ShowPotSearchBar.kt
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/component/ShowPotSearchBar.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
@@ -41,6 +43,7 @@ fun ShowPotSearchBar(
         modifier = modifier
             .fillMaxWidth()
             .height(52.dp)
+            .clip(RoundedCornerShape(2.dp))
             .background(color = ShowpotColor.Gray500)
             .conditional(enabled.not()) {
                 clickable {
