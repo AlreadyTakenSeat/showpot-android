b3a7f70caba6183da1f1aa4cf9d93602de0a368e
soopeach <hsjeon584@gmail.com>
2024-08-30 21:34:57 +0900

    Feat: 알림 설정하기 기능 구현(추후 수정 필요)

--- feature/show-detail/src/main/java/com/alreadyoccupiedseat/show_detail/ShowDetailViewModel.kt
+++ feature/show-detail/src/main/java/com/alreadyoccupiedseat/show_detail/ShowDetailViewModel.kt
@@ -9,9 +9,21 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+sealed interface ShowDetailEvent {
+    data object Idle : ShowDetailEvent
+
+    data object AlertRegisterSuccess : ShowDetailEvent
+}
+
 data class ShowDetailState(
     val showDetail: ShowDetail? = null,
-    val isSheetVisible: Boolean = false
+    val isSheetVisible: Boolean = false,
+    val isFirstItemAvailable: Boolean = false,
+    val isSecondItemAvailable: Boolean = false,
+    val isThirdItemAvailable: Boolean = true,
+    val isFirstItemSelected: Boolean = false,
+    val isSecondItemSelected: Boolean = false,
+    val isThirdItemSelected: Boolean = false,
 )
 
 @HiltViewModel
@@ -22,6 +34,9 @@ class ShowDetailViewModel @Inject constructor(
     private val _state = MutableStateFlow(ShowDetailState())
     val state = _state
 
+    private val _event = MutableStateFlow<ShowDetailEvent>(ShowDetailEvent.Idle)
+    val event = _event
+
     fun getShowDetail(showId: String) {
         viewModelScope.launch {
             val result = showRepository.getShowDetail(showId)
@@ -31,8 +46,9 @@ class ShowDetailViewModel @Inject constructor(
 
     fun registerShowInterest(showId: String) {
         viewModelScope.launch {
-            val isInterested =  showRepository.registerShowInterest(showId)
-            _state.value = _state.value.copy(showDetail = _state.value.showDetail?.copy(isInterested = isInterested))
+            val isInterested = showRepository.registerShowInterest(showId)
+            _state.value =
+                _state.value.copy(showDetail = _state.value.showDetail?.copy(isInterested = isInterested))
         }
     }
 
@@ -42,7 +58,35 @@ class ShowDetailViewModel @Inject constructor(
 
     fun registerTicketingAlert(showId: String, ticketingApiType: String, alertTimes: List<String>) {
         viewModelScope.launch {
-            showRepository.registerTicketingAlert(showId, ticketingApiType, alertTimes)
+            val result = showRepository.registerTicketingAlert(showId, ticketingApiType, alertTimes)
+            if (result.isSuccess) {
+                _event.emit(ShowDetailEvent.AlertRegisterSuccess)
+            } else {
+                println(result.exceptionOrNull())
+            }
         }
     }
+
+    fun changeFirstItemSelection() {
+        _state.value = _state.value.copy(
+            isFirstItemSelected =
+            !state.value.isFirstItemSelected
+        )
+    }
+
+    fun changeSecondItemSelection() {
+        _state.value = _state.value.copy(
+            isSecondItemSelected =
+            !state.value.isSecondItemSelected
+        )
+    }
+
+    fun changeThirdItemSelection() {
+        _state.value = _state.value.copy(
+            isThirdItemSelected =
+            !state.value.isThirdItemSelected
+        )
+    }
+
+    // TODO: generate fun to change the avialability of the first, second, and third item
 }
