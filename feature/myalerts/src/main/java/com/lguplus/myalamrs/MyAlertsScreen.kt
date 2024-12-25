67dcc16894eea534b09fd78bb267b281f56f2a0c
hyeonseok <startwo09@gmail.com>
2024-12-25 18:55:45 +0900

    Feat: Chore: 알림 리스트 화면 이동 이벤트 추가와 기본 TopBar UI 작업

--- feature/myalerts/src/main/java/com/lguplus/myalamrs/MyAlertsScreen.kt
+++ feature/myalerts/src/main/java/com/lguplus/myalamrs/MyAlertsScreen.kt
@@ -1,18 +1,50 @@
 package com.lguplus.myalamrs
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.alreadyoccupiedseat.designsystem.ShowpotColor
 
 @Composable
-fun MyAlarmsScreen(
-    modifier: Modifier = Modifier
+fun MyAlertsScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
 ) {
-    MyAlarmsContentScreen()
+    MyAlertsContentScreen(
+        navController = navController,
+        onBackClicked = {
+            navController.popBackStack()
+        }
+    )
 }
 
 @Composable
-fun MyAlarmsContentScreen(
-    modifier: Modifier = Modifier
+private fun MyAlertsContentScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    onBackClicked: () -> Unit,
 ) {
+    Scaffold(
+        containerColor = ShowpotColor.Gray700,
+        topBar = {
+            MyAlertsTopBar { onBackClicked() }
+        },
+        content = {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier
+                    .padding(top = 12.dp)
+                    .padding(it),
+            ) {
 
+            }
+        }
+    )
 }
