da1688e0e385f38fb090ac2becd7bf6f054186b0
hyeonseok <startwo09@gmail.com>
2024-12-27 01:54:59 +0900

    Chore: 서버 통신 Result 적용

--- core/data/src/main/java/com/alreadyoccupiedseat/data/alert/AlertDataSourceImpl.kt
+++ core/data/src/main/java/com/alreadyoccupiedseat/data/alert/AlertDataSourceImpl.kt
@@ -1,14 +1,19 @@
 package com.alreadyoccupiedseat.data.alert
 
+import com.alreadyoccupiedseat.data.getResult
 import com.alreadyoccupiedseat.model.Alert
 import com.alreadyoccupiedseat.network.AlertService
 import javax.inject.Inject
 
 class AlertDataSourceImpl @Inject constructor(
-private val alertService: AlertService
+    private val alertService: AlertService,
 ) : AlertDataSource {
-    override suspend fun getAlerts(cursorId: String?, size: Int): List<Alert> {
-        return alertService.getAlerts(cursorId, size).body()?.data?.data ?: emptyList()
+    override suspend fun getAlerts(cursorId: String?, size: Int): Result<List<Alert>> {
+        return runCatching {
+            alertService.getAlerts(cursorId, size).getResult {
+                it.data.data
+            }
+        }
     }
 
     override suspend fun getAlertsExist(): Boolean {
