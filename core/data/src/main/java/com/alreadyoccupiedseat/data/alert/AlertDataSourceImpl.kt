cf6ecacb09a5afe11ef7c146058a5dc6664b5e7b
hyeonseok <startwo09@gmail.com>
2024-12-27 01:09:41 +0900

    Chore 불필요한 Context 주입 제거

--- core/data/src/main/java/com/alreadyoccupiedseat/data/alert/AlertDataSourceImpl.kt
+++ core/data/src/main/java/com/alreadyoccupiedseat/data/alert/AlertDataSourceImpl.kt
@@ -1,14 +1,11 @@
 package com.alreadyoccupiedseat.data.alert
 
-import android.content.Context
 import com.alreadyoccupiedseat.model.Alert
 import com.alreadyoccupiedseat.network.AlertService
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class AlertDataSourceImpl @Inject constructor(
-private val alertService: AlertService,
-@ApplicationContext private val context: Context
+private val alertService: AlertService
 ) : AlertDataSource {
     override suspend fun getAlerts(cursorId: String?, size: Int): List<Alert> {
         return alertService.getAlerts(cursorId, size).body()?.data?.data ?: emptyList()
