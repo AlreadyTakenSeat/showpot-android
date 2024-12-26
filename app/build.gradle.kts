5b65e702a46e8869dd9cc453d45131b94dc0b19b
soopeach <hsjeon584@gmail.com>
2024-12-26 19:07:44 +0900

    Chore: 버전 0.9.1로 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.alreadyoccupiedseat.showpot"
         targetSdk = 34
         versionCode = 2
-        versionName = "0.9.0"
+        versionName = "0.9.1"
 
         // for using it on the .kt files
         buildConfigField(
