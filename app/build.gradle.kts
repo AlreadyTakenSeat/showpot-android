0f626fa7d92f376544683fa828186744aa241694
hyeonseok <startwo09@gmail.com>
2025-01-02 00:13:58 +0900

    Chore: 버전 0.9.5로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.alreadyoccupiedseat.showpot"
         targetSdk = 34
         versionCode = 2
-        versionName = "0.9.4"
+        versionName = "0.9.5"
 
         // for using it on the .kt files
         buildConfigField(
