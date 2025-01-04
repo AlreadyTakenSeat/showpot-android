593424c988b3c0b5b231178eb7bd5874a9d0bde5
soopeach <hsjeon584@gmail.com>
2025-01-04 13:46:24 +0900

    feat: 로딩 인디케이터 구현
    - warning: 추후 이미지 리소스 변경

---
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/component/loading/LoadingIndicator.kt
@@ -0,0 +1,18 @@
+package com.alreadyoccupiedseat.designsystem.component.loading
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.alreadyoccupiedseat.designsystem.R
+
+@Composable
+fun LoadingIndicator() {
+    RotatingImage(
+        modifier = Modifier.size(40.dp)
+            .padding(1.dp),
+        painter = painterResource(R.drawable.img_loading_indicator)
+    )
+}
