5dd8e65b2d0f0524113981221a68a87cf99e3c62
soopeach <hsjeon584@gmail.com>
2025-01-23 17:36:53 +0900

    chore: 버전 7, 1.0.4로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     defaultConfig {
         applicationId = "com.alreadyoccupiedseat.showpot"
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.3"
+        versionCode = 7
+        versionName = "1.0.4"
 
         // for using it on the .kt files
         buildConfigField(
