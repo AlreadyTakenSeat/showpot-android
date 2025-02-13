052a77fbd6138332956fcacd55f313166ff2ad6e
soopeach <hsjeon584@gmail.com>
2025-02-13 16:07:53 +0900

    chore: 임시로 사용할 공연 더미 데이터 추가

--- model/src/main/java/com/alreadyoccupiedseat/model/comment/CommentResponse.kt
+++ model/src/main/java/com/alreadyoccupiedseat/model/comment/CommentResponse.kt
@@ -11,4 +11,25 @@ data class CommentResponse(
     val parentId: String,
     val profileURL: String,
     val userName: String
-)
+) {
+    companion object {
+
+        private var dummyCommentId = 1
+        private var dummyParentId = 1
+        val Dummy
+            get() = CommentResponse(
+            commentId = dummyCommentId++.toString(),
+            content = listOf(
+                "안녕하세요",
+                "반가워요",
+                "잘 지내세요",
+                "댓글을 달아주세요",
+            ).shuffled().first(),
+            createdAt = "2021-09-01T00:00:00",
+            isBlocked = false,
+            parentId = dummyParentId++.toString(),
+            profileURL = "https://picsum.photos/30",
+            userName = "Hi"
+        )
+    }
+}
